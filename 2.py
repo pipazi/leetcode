42c65ff6b10a8acce4498f23af438db4ee81b67c
pipazi <42357044+pipazi@users.noreply.github.com>
2020-10-12 13:03:38 +0800

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,29 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        a = l3 = ListNode(0)
+        quo = 0
+        while l1 or l2:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+
+            res = x + y + quo
+            rem = res if res < 10 else res - 10
+            quo = 0 if res < 10 else 1
+
+            l3.next = ListNode(rem)
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+            l3 = l3.next
+
+        if quo:
+            l3.next = ListNode(quo)
+        return a.next
