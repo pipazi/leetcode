5ff5948dad479e3aaa434d2778c5aebd904dcfea
pipazi <42357044+pipazi@users.noreply.github.com>
2021-04-27 23:02:19 +0800

    Create 87.py

---
+++ 87.py
@@ -0,0 +1,17 @@
+import functools
+class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+        @functools.lru_cache()
+        def dfs(str1, str2):
+            if str1 == str2:
+                return True
+            for ch in set(str1):
+                if str1.count(ch) != str2.count(ch):
+                    return False
+            for i in range(1, len(str1)):
+                if dfs(str1[:i], str2[:i]) and dfs(str1[i:], str2[i:]):
+                    return True
+                if dfs(str1[:i], str2[-i:]) and dfs(str1[i:], str2[:-i]):
+                    return True
+            return False
+        return dfs(s1, s2)
